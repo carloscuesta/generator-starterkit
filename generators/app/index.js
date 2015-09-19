dc3a47987a8ba5b947efb5da42724b319982238e
Carlos Cuesta <carloscuesta@me.com>
2015-09-20 00:12:13 +0200

    setting up yeoman
    creating functions at index.js to start with the starterkit generator
    and working with the lodash templating system and files.

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,67 +5,127 @@ var yeoman = require('yeoman-generator'),
     mkdirp = require('mkdirp');
 
 module.exports = yeoman.generators.Base.extend({
+
+    _projectStructure: function() {
+        var destRoot = this.destinationRoot(),
+            distDir = destRoot + '/dist/',
+            srcDir = destRoot + '/src/',
+            assetsDir = distDir + '/assets/';
+
+        mkdirp(distDir);
+        mkdirp(assetsDir + '/css');
+        mkdirp(assetsDir + '/js');
+        mkdirp(assetsDir + '/files/images');
+
+        mkdirp(srcDir);
+        mkdirp(srcDir + '/images');
+        mkdirp(srcDir + '/scripts');
+        mkdirp(srcDir + '/styles/_includes/');
+        mkdirp(srcDir + '/templates/_includes/');
+    },
+
+    _projectFiles: function() {
+        var templateCtx = {
+            appname: this.appname,
+            appversion: this.appversion,
+            appdescription: this.appdescription,
+            applicense: this.applicense,
+            appauthor: this.appauthor,
+            appemail: this.appemail
+        };
+
+        this.fs.copy(
+            this.templatePath('editorconfig'),
+            this.destinationPath('.editorconfig')
+        );
+
+        this.fs.copy(
+            this.templatePath('jshintrc'),
+            this.destinationPath('.jshintrc')
+        );
+
+        this.fs.copy(
+            this.templatePath('gulpfile.js'),
+            this.destinationPath('gulpfile.js')
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('_package.json'),
+            this.destinationPath('package.json'),
+            templateCtx
+        );
+
+        this.fs.copyTpl(
+            this.templatePath('_bower.json'),
+            this.destinationPath('bower.json'),
+            templateCtx
+        );
+    },
+
+    _askUser: function() {
+        var answers = [
+            {
+                name: 'name',
+                message: 'What is the name of your project',
+                default: this.appname
+            },
+            {
+                name: 'description',
+                message: 'What is the description of your project'
+            },
+            {
+                name: 'version',
+                message: 'What is the version of your project',
+                default: '0.0.0'
+            },
+            {
+                name: 'license',
+                message: 'How is your project licensed',
+                default: 'MIT'
+            },
+            {
+                name: 'name',
+                message: 'What is your name'
+            },
+            {
+                name: 'email',
+                message: 'What is your email address'
+            },
+        ];
+        return answers;
+    },
+
+    _getAnswers: function(answers, callback) {
+        this.appname = answers.name;
+        this.appdescription = answers.appdescription;
+        this.appversion = answers.version;
+        this.applicense = answers.license;
+        this.appauthor = answers.yourname;
+        this.appemail = answers.email;
+        callback();
+    },
+
+    initializing: function() {
+        var greeting = 'Welcome to the ' + chalk.red.bold('starterkit') + '!' + ' A solid ' + chalk.blue('webkit') + ' to develop at the' + chalk.yellow(' front end');
+
+        this.log(yosay(greeting, {
+            maxLength: 26
+        }));
+    },
+
     prompting: function() {
         var done = this.async();
 
-        // Have Yeoman greet the user.
-        this.log(yosay(
-            'Welcome to the fabulous ' + chalk.red('Starterkit') + ' generator!'
-        ));
-
-        var prompts = [{
-            name: 'projectName',
-            message: 'What do you want to call your project?'
-        }];
-
-        this.prompt(prompts, function(props) {
-            this.projectName = props.projectName;
-            console.log(this.projectName);
+        this.prompt(this._askUser(), function(answers) {
+            this._getAnswers(answers, done);
             done();
         }.bind(this));
     },
 
-    writing: {
-        directories: function() {
-            mkdirp('dist/assets/css');
-            mkdirp('dist/assets/files/img/');
-            mkdirp('dist/assets/js/');
-            mkdirp('src/images/');
-            mkdirp('src/scripts/');
-            mkdirp('src/styles/_includes/');
-            mkdirp('src/templates/_includes/');
-        },
-
-        app: function() {
-            this.fs.copy(
-                this.templatePath('_package.json'),
-                this.destinationPath('package.json')
-            );
-
-            this.fs.copy(
-                this.templatePath('_bower.json'),
-                this.destinationPath('bower.json')
-            );
-        },
-
-        projectfiles: function() {
-            this.fs.copy(
-                this.templatePath('editorconfig'),
-                this.destinationPath('.editorconfig')
-            );
-
-            this.fs.copy(
-                this.templatePath('jshintrc'),
-                this.destinationPath('.jshintrc')
-            );
-
-            this.fs.copy(
-                this.templatePath('gulpfile.js'),
-                this.destinationPath('gulpfile.js')
-            );
-        }
+    writing: function() {
+        this._projectStructure();
+        this._projectFiles();
     },
-
 
     install: function() {
         this.installDependencies();
