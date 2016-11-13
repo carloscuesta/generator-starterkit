6719689062e54aa3dd0ee822c5d6db0c3297620a
Carlos Cuesta <carloscuesta@me.com>
2016-11-13 09:47:32 +0100

    :rotating_light:

--- generators/app/index.es6.js
+++ generators/app/index.es6.js
@@ -221,7 +221,7 @@ class Starterkit extends Base {
 			name: 'email',
 			message: 'What is your email address',
 			validate(value) {
-				const validEmail = value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+				const validEmail = value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 				if (validEmail) {
 					return true;
 				}
